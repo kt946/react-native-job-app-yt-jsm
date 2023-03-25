76ab54a71871f6019521e7bb4222bb7b823eb4fa
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-03-25 12:44:27 -0400

    updated ScreenHeaderBtn

--- app/index.js
+++ app/index.js
@@ -22,7 +22,7 @@ const Home = () => {
           ),
           headerRight: () => (
             <ScreenHeaderBtn
-              iconUrl={icons.profile}
+              iconUrl={images.profile}
               dimension="100%"
             />
           ),
