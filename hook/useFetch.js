8a28539d45d443aa757a2d2f49c41a48c912d12d
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-03-26 15:28:06 -0400

    added rapidApi

---
+++ hook/useFetch.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: 'GET',
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    params: { ...query },
+    headers: {
+      'X-RapidAPI-Key': '241f630d40msh768c0561d9b9eb6p18a165jsnb75d228a5ff3',
+      'X-RapidAPI-Host': 'jsearch.p.rapidapi.com',
+    },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert('There is an error');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
