411856149f73ccb25b5c70a44d0c3fe91c7c56a7
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-03-27 13:50:07 -0400

    updated job details page, about, and specifics components

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -17,6 +17,29 @@ const JobDetails = () => {
 
   const onRefresh = () => {};
 
+  const displayTabContent = () => {
+    switch (activeTab) {
+      case 'Qualifications':
+        return (
+          <Specifics
+            title="Qualifications"
+            points={data[0].job_highlights?.Qualifications ?? ['N/A']}
+          />
+        );
+      case 'About':
+        return <JobAbout info={data[0].job_description ?? 'No data provided'} />;
+      case 'Responsibilities':
+        return (
+          <Specifics
+            title="Responsibilities"
+            points={data[0].job_highlights?.Responsibilities ?? ['N/A']}
+          />
+        );
+      default:
+        break;
+    }
+  };
+
   const { data, isLoading, error, refetch } = useFetch('job-details', {
     job_id: params.id,
   });
@@ -78,6 +101,8 @@ const JobDetails = () => {
                 active={activeTab}
                 setActiveTab={setActiveTab}
               />
+
+              {displayTabContent()}
             </View>
           )}
         </ScrollView>
